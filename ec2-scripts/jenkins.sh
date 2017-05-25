75c0eafda525fdfef692955650e093e1dbdc3fea
eschweit-at-tibco <eschweit@tibco.com>
2017-05-26 05:18:26 +1000

    Update jenkins.sh

--- ec2-scripts/jenkins.sh
+++ ec2-scripts/jenkins.sh
@@ -2,3 +2,6 @@
 
 sed 's/JENKINS_HOME=.*$/JENKINS_HOME=\"\/jenkins\"/;s/JENKINS_JAVA_OPTIONS=\"/&-Djenkins.install.runSetupWizard=false /' /etc/sysconfig/jenkins > /etc/sysconfig/jenkins.new
 mv -f /etc/sysconfig/jenkins.new /etc/sysconfig/jenkins
+
+sed "s/##PWD##/${1}/" /jenkins/init.groovy > /tmp/init.groovy
+mv -f /tmp/init.groovy /jenkins/init.groovy
