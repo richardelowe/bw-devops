bf41eb60fc26c3e684c52ee52a4a561c20dc734d
eschweit-at-tibco <eschweit@tibco.com>
2017-05-26 10:45:53 +1000

    Moved jenkins plugin installation into init.groovy

--- ec2-scripts/init.groovy
+++ ec2-scripts/init.groovy
@@ -11,3 +11,30 @@ instance.setSecurityRealm(hudsonRealm)
 def strategy = new FullControlOnceLoggedInAuthorizationStrategy()
 instance.setAuthorizationStrategy(strategy)
 instance.save()
+
+def installed = false
+def initialised = false
+def pluginsString = "git s3 jenkins-cloudformation-plugin build-pipeline-plugin dashboard-view workflow-aggregator"
+def plugins = pluginsString.split()
+def pm = instance.getPluginManager()
+def uc = instance.getUpdateCenter()
+
+plugins.each() {
+  if (!pm.getPlugin(it)) {
+    if (!initialised) {
+      uc.updateAllSites()
+      initialised = true
+    }
+    
+    def plugin = uc.getPlugin(it)
+    if (plugin) {
+      plugin.deploy()
+      installed = true
+    }
+  }
+}
+
+if (installed) {
+  instance.save()
+//  instance.doSafeRestart()
+}
