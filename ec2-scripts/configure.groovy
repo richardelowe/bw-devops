d3b6fae4c00243f11f18ebe1f04e1be5da6749b2
eschweit-at-tibco <eschweit@tibco.com>
2017-06-01 19:32:23 +1000

    Update configure.groovy

--- ec2-scripts/configure.groovy
+++ ec2-scripts/configure.groovy
@@ -7,22 +7,24 @@ import com.cloudbees.plugins.credentials.CredentialsScope
 import hudson.util.Secret
 import org.jenkinsci.plugins.github.config.*
 
+def instance = Jenkins.getInstance()
+
 // configure maven
-define mavenPluginExtension = instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
-define mavenList = (mavenPluginExtension.installations as List);
+def mavenPluginExtension = instance.getExtensionList(hudson.tasks.Maven.DescriptorImpl.class)[0];
+def mavenList = (mavenPluginExtension.installations as List);
 mavenList.add(new hudson.tasks.Maven.MavenInstallation("M3", "/usr/share/maven", []));
 mavenPluginExtension.installations = mavenList
-mavenPluginExtesion.save()
+mavenPluginExtension.save()
 
 // configure credentials for github
 def credentials = new StringCredentialsImpl(CredentialsScope.GLOBAL,
                                             UUID.randomUUID().toString(),
                                             "Access to GitHub",
-                                            Secret.fromString("##GHTOKEN##"))
+                                            Secret.fromString("1d3da6d0a4e2d9b27b82a5a57575f05f3f8c1157"))
 
 def credentialsStore = jenkins.model.Jenkins.instance.getExtensionList(com.cloudbees.plugins.credentials.SystemCredentialsProvider.class)[0]
-def globalDomain = com.cloudbees.plugins.credentials.domains.Domain.getGlobal()
-credentialsStore.storeImpl.addCredentials(globalDomain, credentials)
+def globalDomain = com.cloudbees.plugins.credentials.domains.Domain.global()
+credentialsStore.addCredentials(globalDomain, credentials)
 credentialsStore.save()
 
 // configure github server
