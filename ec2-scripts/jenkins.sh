8325004f21603a86c9d2fc2e92fb34d980bf71d6
eschweit-at-tibco <eschweit@tibco.com>
2017-06-07 10:11:53 +1000

    Added maven

--- ec2-scripts/jenkins.sh
+++ ec2-scripts/jenkins.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 
-# download the jenkins package
+# install maven
+yum -y install maven > /tmp/yum-maven.log 2>&1
+
+# download and install the jenkins package
 wget -O /etc/yum.repos.d/jenkins.repo http://pkg.jenkins-ci.org/redhat/jenkins.repo
 rpm --import http://pkg.jenkins-ci.org/redhat/jenkins-ci.org.key
 yum -y install jenkins > /tmp/yum-jenkins.log 2>&1
