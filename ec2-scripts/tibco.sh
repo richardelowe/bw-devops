1d153ff3244c6be881930409da52c62cc4c92b80
eschweit-at-tibco <eschweit@tibco.com>
2017-06-13 17:51:35 +1000

    Create tibco.sh

--- ec2-scripts/tibco.sh
+++ ec2-scripts/tibco.sh
@@ -3,6 +3,7 @@
 export GIT_TIB_URL=https://raw.githubusercontent.com/TIBCOSoftware/bw6-plugin-maven
 
 # install bw/bwce maven plugin
+cd /tmp
 wget --no-check-certificate --content-disposition ${GIT_TIB_URL}/master/Installer/TIB_BW_Maven_Plugin_1.2.1.zip
 unzip TIB_BW_Maven_Plugin_1.2.1.zip -d TIB_BW_Maven
 cd TIB_BW_Maven
@@ -16,10 +17,10 @@ cd /opt/tibco/bw/6.3/maven/plugins/bw6-maven-plugin
 cd /opt/tibco
 mkdir bwce
 cd bwce
-wget $1
+wget --no-check-certificate --content-disposition $1
 unzip bwce_docker.zip
 cd docker/resources/bwce-runtime
-wget $2
+wget --no-check-certificate --content-disposition $2
 docker build -t bwce:latest .
 
 chown -R jenkins:jenkins /opt/tibco
