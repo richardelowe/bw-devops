d539ed0ad8df98c2c79b2882c4d7a141ba578b1f
eschweit-at-tibco <eschweit@tibco.com>
2017-06-14 12:10:15 +1000

    Create setup-pipeline.groovy

--- ec2-scripts/setup-pipeline.groovy
+++ ec2-scripts/setup-pipeline.groovy
@@ -1,36 +1,8 @@
 #!groovy
 
 import jenkins.model.*
+import java.io.*
 
-node {
-    def mvnHome
-    stage('Preparation') { // for display purposes
-        // Get some code from a GitHub repository
-        git 'https://github.com/eschweit-at-tibco/bookstore.git'
-        mvnHome = tool 'M3'
-    }
-    
-    stage('Build') {
-        // Run the maven build
-        dir('tibco.bwce.sample.binding.rest.BookStore.application.parent') {
-            if (isUnix()) {
-                sh "'${mvnHome}/bin/mvn' -Dmaven.test.failure.ignore clean package"
-            } else {
-                bat(/"${mvnHome}\bin\mvn" -Dmaven.test.failure.ignore clean package/)
-            }
-        }
-    }
-    
-    stage('Testing') {
-        dir('tibco.bwce.sample.binding.rest.BookStore.application.parent') {
-            if (isUnix()) {
-                sh "ant build deploy-test-image newman undeploy-test-image"
-            }
-        }
-    }
-    
-    stage('Results') {
-        // junit '**/target/surefire-reports/TEST-*.xml'
-        // archive 'target/*.jar'
-    }
-}
+def source = new FileInputStream('/tmp/pipeline.xml')
+def tli = Jenkins.instance.createProjectFromXML("BW-Bookstore", source)
+tli.save()
